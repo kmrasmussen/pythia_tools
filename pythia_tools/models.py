6af47ef6c7b11ec978e209605c0b0bf47505d084
Kasper M. Rasmussen <obscur@tuta.io>
2023-04-13 22:35:15 +0200

    get_n_layers from only name

--- pythia_tools/models.py
+++ pythia_tools/models.py
@@ -10,6 +10,12 @@ from transformer_lens.hook_points import HookedRootModule, HookPoint
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model_names = ['70m', '160m', '410m', '1b']
+n_layers_dict = {
+    '70m': 6,
+    '160m': 12,
+    '410m': 12,
+    '1b': 24
+}
 
 def get_param_sizes_list():
     param_sizes = [7e7, 1.6e8, 4.1e8, 1e9]
@@ -27,13 +33,17 @@ def get_model(size, rev, lens=False):
         model = GPTNeoXForCausalLM.from_pretrained(f"EleutherAI/pythia-{size}-deduped", revision=f"step{rev}000", cache_dir=f"./pythia-{size}-deduped/step{rev}000").to(device)
     return model
 
-def yield_models(lens=False):
-    for model_name in model_names:
+def yield_models(names=None, lens=False):
+    if names is None: 
+        names = model_names
+    for model_name in names:
         model = get_model(model_name, 143, lens)
         yield (model_name, model)
 
 def get_n_layers(model):
-    if isinstance(model, GPTNeoXForCausalLM):
+    if isinstance(model, str):
+        return n_layers_dict[model]
+    elif isinstance(model, GPTNeoXForCausalLM):
         return model.config.num_hidden_layers
     else:
         return len(model.blocks)
